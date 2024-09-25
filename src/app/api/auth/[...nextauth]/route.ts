ce400ad1f2685e3806bb4204d9b4e581b898cba6
subhraneel2005 <digitalsubhraneel@gmail.com>
2024-09-25 22:09:27 +0530

    last try 2

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,7 @@
 
 import { authOptions } from "@/providers/auth";
-import NextAuth from "next-auth";
+import NextAuth from "next-auth/next";
+
 
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
