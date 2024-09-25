dccc7ac3503e53458f31d63333d7bd25a81d4178
subhraneel2005 <digitalsubhraneel@gmail.com>
2024-09-25 22:19:15 +0530

    bug removed

--- src/providers/session.js
+++ src/providers/session.js
@@ -1,5 +1,6 @@
 import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "./auth";
+
 
 export async function getCurrentUser() {
   const session = await getServerSession(authOptions);
