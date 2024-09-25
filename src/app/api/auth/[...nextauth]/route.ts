4402f7bf4cca379b98e223acd1b25c528a4ace62
subhraneel2005 <digitalsubhraneel@gmail.com>
2024-09-25 22:11:41 +0530

    plis bug go away

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,26 @@
+import NextAuth, { AuthOptions } from 'next-auth';
+import GithubProvider from 'next-auth/providers/github';
+import GoogleProvider from 'next-auth/providers/google';
+import { PrismaAdapter } from '@auth/prisma-adapter';
+import prisma from '@/prisma';
 
-import { authOptions } from "@/providers/auth";
-import NextAuth from "next-auth/next";
-
+ const authOptions: AuthOptions = {
+  pages: {
+    signIn: '/login',
+  },
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID!,
+      clientSecret: process.env.GOOGLE_SECRET!,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET!,
+};
 
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
